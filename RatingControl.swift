081e8edbec186923aea01dff605079818b2a5c67
Hanna Holovach <ann.golovach@gmail.com>
2015-10-06 13:29:55 +0300

    two dots crashed my code 😐

--- RatingControl.swift
+++ RatingControl.swift
@@ -16,11 +16,12 @@ class RatingControl: UIView {
     // MARK: Initialization
 
     required init?(coder aDecoder: NSCoder) {
+        print("Startin the party🎉")
         super.init(coder: aDecoder)
         
         let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
         button.backgroundColor = UIColor.redColor()
-        button.addTarget(self, action: "ratingButtonTapped", forControlEvents: .TouchDown)
+        button.addTarget(self, action: "ratingButtonTapped:", forControlEvents: .TouchDown)
         addSubview(button)
     }
     
